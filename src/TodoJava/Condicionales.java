3eee0c001df725d71dd0c8dce9e82b09a5140b5f
AlanIsaacToroHolguin <alan.software.arch@gmail.com>
2025-12-29 15:29:31 -0500

    Java Conditionals

--- src/TodoJava/Condicionales.java
+++ src/TodoJava/Condicionales.java
@@ -4,14 +4,19 @@ public class Condicionales {
 
     public static void run(){
 
-        int num1 = 10;
-        int num2 = 5;
+        int num1 = 15;
+        int num2 = 15;
 
         if (num2 > num1){
-            System.out.println("El " + num2 + " es mayor al " + num1);
+            System.out.println("El " + num2 + " es un numero mayor al " + num1);
              }
             else{
-            System.out.println("El " + num2 + " no es mayor al " + num1);
+                if (num1 == num2){
+                    System.out.println("El " + num2 + " es un numero  igual al  " + num1);
+                }
+                else{
+                    System.out.println("El " + num2 + " no es un numero mayor al " + num1);
+                }
             }
 
 
