c57b88057f5d6ef3c435864a0908b2484deca15d
AlanIsaacToroHolguin <alan.software.arch@gmail.com>
2025-12-30 09:38:48 -0500

    Java For

--- src/TodoJava/For.java
+++ src/TodoJava/For.java
@@ -5,7 +5,7 @@ public class For {
     public static void run(){
 
         for (int cont = 0; cont<=10; cont++){
-            System.out.println("Estoy dando vueltas con for, voy en la vuelta "+ cont);
+            System.out.println("Estoy dando vueltas con for, estoy  en la vuelta "+ cont);
         }
     }
 }
